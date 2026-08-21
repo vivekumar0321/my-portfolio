c2baed1c5edc839f09e4963d488d1a1295365b60
vivekumar0321 <vivekumar4525@gmail.com>
2026-08-21 16:06:18 +0530

    update code

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -81,7 +81,7 @@ define(['./workbox-7e5eb42b'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.lsj153clmu4"
+    "revision": "0.37tcg5a34mk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
